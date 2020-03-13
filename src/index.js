d4c22f8284800a7bba30f9081339a742f83d3cc0
feelic <feelics@gmail.com>
2020-03-13 23:19:12 +0100

    handle file upload

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import express from "express";
 import cors from "cors";
+import uuid from 'uuid';
 import bodyParser from "body-parser";
+import fileUpload from "express-fileupload";
 import * as actions from "./db/actions.js";
+import path from 'path';
 
 const app = express();
 const port = 3000;
+const uuidv4 = uuid.v4;
 
 app.use(cors());
-
+app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(
+  fileUpload({
+    createParentPath: true
+  })
+);
 
 // READ ALL CHARACTERS
 app.get("/characters", (req, res) => {
@@ -30,8 +39,8 @@ app.post("/character", (req, res) => {
 
   actions
     .insertCharacters([character])
-    .then((data) => {
-      res.json({data: data.ops[0]});
+    .then(data => {
+      res.json({ data: data.ops[0] });
     })
     .catch(handleError.bind(res));
 });
@@ -61,7 +70,7 @@ app.post("/character/:charId", (req, res) => {
   actions
     .updateCharacter(charId, character)
     .then(() => {
-      return findCharacter(charId);
+      return actions.findCharacter(charId);
     })
     .then(data => {
       res.json({ data: data[0] });
@@ -80,11 +89,41 @@ app.delete("/character/:charId", (req, res) => {
     })
     .catch(handleError.bind(res));
 });
+app.post("/image-upload", async (req, res) => {
+  try {
+    if (!req.files) {
+      res.send({
+        status: false,
+        message: "No file uploaded"
+      });
+    } else {
+      //Use the name of the input field (i.e. "avatar") to retrieve the uploaded file
+      const image = req.files[0];
+      const extension = path.extname(image.name);
+      const newName = uuidv4() + extension;
+      //Use the mv() method to place the file in upload directory (i.e. "uploads")
+      image.mv("./public/images/" + newName);
+
+      //send response
+      res.send({
+        status: true,
+        message: "File is uploaded",
+        data: {
+          name: newName,
+          mimetype: image.mimetype,
+          size: image.size
+        }
+      });
+    }
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
 
 app.listen(port, () =>
   console.log(`Hello world app listening on port ${port}!`)
 );
 
 function handleError(err) {
-  this.json({ error: "API encountered an error", details: err });
+  this.status(500).json({ error: "API encountered an error", details: err });
 }
